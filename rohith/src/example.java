662e3af721b94d9bb11822a401806c506671bb5d
kavin.s <kkavin7513@gmail.com>
2025-02-25 11:04:19 +0530

    first commit

--- rohith/src/example.java
+++ rohith/src/example.java
@@ -1,4 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    
+    // declare variables
+    int a = 12, b = 5;
 
-public class example {
+    // addition operator
+    System.out.println("a + b = " + (a + b));
 
+    // subtraction operator
+    System.out.println("a - b = " + (a - b));
+
+    // multiplication operator
+    System.out.println("a * b = " + (a * b));
+
+    // division operator
+    System.out.println("a / b = " + (a / b));
+
+    // modulo operator
+    System.out.println("a % b = " + (a % b));
+  }
 }
